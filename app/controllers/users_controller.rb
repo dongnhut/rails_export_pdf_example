ef834c2d8f4c106f5af8bd555a76ba59a62f4eee
Vo Dong Nhut <nhutvd@wakumo.vn>
2017-09-27 17:24:28 +0700

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,96 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+   @user =  User.find(params[:id])
+
+   respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "user#{@user.id}.pdf",   # Excluding ".pdf" extension.
+          :disposition => "inline",
+          :template => "users/show.pdf.erb",
+          :layout => "pdf_layout.html.erb"
+      end
+    end
+
+  end
+
+  def export
+    @user =  User.find(params[:id])
+    html = render_to_string(:action => :show, :layout => "pdf_layout.html.erb")
+    pdf = WickedPdf.new.pdf_from_string(html)
+
+    send_data(pdf,
+      :filename => "my_pdf_name.pdf",
+      :disposition => 'attachment')
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.fetch(:user, {})
+    end
+end
